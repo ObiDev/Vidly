ba4e11e595b56b9bfa8b5d59a55197281e807f2e
maobi <marcin.obijalski@consolis.com>
2020-06-15 19:48:52 +0200

    Add DbContext to CustomerControler

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -4,36 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            List<Customer> customers = GetCustomers();
+            IEnumerable<Customer> customers = _context.Customers.Include(m => m.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(m => m.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private List<Customer> GetCustomers()
-        {
-            return new List<Customer>()
-            {
-                new Customer() { Id = 1, Name = "John Smith" },
-                new Customer() { Id = 2, Name = "Mary Wiliams" }
-            };
         }
     }
 }
