38b5c83477a0e0030a8880070feac62ddd241118
ObiDev <marcinobi@gmail.com>
2020-07-17 14:30:24 +0200

    Custom validation applied

---
+++ Models/Min18YearsIfAMember.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == MembershipType.Unknown || customer.MembershipTypeId == MembershipType.PayAsYouGo)
+                return ValidationResult.Success;
+
+            if (customer.Birthday == null)
+                return new ValidationResult("Birthday date is required!");
+
+            var age = DateTime.Now.Year - customer.Birthday.Value.Year;
+
+            return (age >= 18) 
+                ? ValidationResult.Success 
+                : new ValidationResult("At least 18 years is required!");
+        }
+    }
+}
