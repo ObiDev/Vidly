c9a2d1f063e0e5872e53c2539fbc393f72873318
ObiDev <marcinobi@gmail.com>
2020-07-17 13:13:17 +0200

    Simple validation applied

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -10,7 +10,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
        
-        [Required]
+        [Required(ErrorMessage ="Podaj nazwę użytkownika!")]
         [StringLength(255)]
         public string Name { get; set; }
         
